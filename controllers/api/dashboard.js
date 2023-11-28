5fbb51dff66d774127a621efecd70c90c26ff2cb
dyl124 <142150017+dyl124@users.noreply.github.com>
2023-11-28 21:54:57 +1100

    USER login

--- controllers/api/dashboard.js
+++ controllers/api/dashboard.js
@@ -1,7 +1,4 @@
 const router = require('express').Router();
-const { Expense } = require('../models');
-const { Income } = require('../models');
-const { User } = require('../models');
 
 // Additional Requirements - such as Models
 // remember to get custom middleware if needed:
