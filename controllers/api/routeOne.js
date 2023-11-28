19281820f9c8f8874ac3ae3246584733b7592ce1
trnigg <tim@nigg.com.au>
2023-11-28 13:02:06 +1100

    Added userAuth middleware and boiler for helpers

--- controllers/api/routeOne.js
+++ controllers/api/routeOne.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 // Additional Requirements - such as Models
+// remember to get custom middleware if needed:
+// const userAuth = require('../utils/auth');
 
 // router. routes
 
