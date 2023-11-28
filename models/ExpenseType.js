230a83b853ecea210dec1060f0b04d432adc64c3
trnigg <tim@nigg.com.au>
2023-11-28 19:40:16 +1100

    Added ExpenseType and Vendor models, updated Expense model

---
+++ models/ExpenseType.js
@@ -0,0 +1,30 @@
+// models/ExpenseType.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class ExpenseType extends Model {}
+
+ExpenseType.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // What other fields do we need specific to ExpenseType, if any?
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'expense_type',
+  }
+);
+
+module.exports = ExpenseType;
---
+++ models/Vendor.js
@@ -0,0 +1,30 @@
+// models/Vendor.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vendor extends Model {}
+
+Vendor.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // What other fields do we need specific to ExpenseType, if any? Do we want vendor details like address etc?
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vendor',
+  }
+);
+
+module.exports = Vendor;
