f914b9046154f37d56cf67aae5abb1e3a31f5725
trnigg <tim@nigg.com.au>
2023-11-28 20:00:07 +1100

    Added Client and IncomeType models, updated Income model

---
+++ models/Client.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Client extends Model {}
+
+Client.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // What other fields do we need specific to Client, if any? Do we want vendor details like address etc?
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'client',
+  }
+);
+
+module.exports = Client;
