49ed24a08197475fb58fe6a78e0908fa256c93e1
trnigg <tim@nigg.com.au>
2023-11-28 21:32:00 +1100

    Added seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,32 @@
 const sequelize = require('../config/connection');
-const { TableOne, TableTwo } = require('../models');
+const { User, Income, IncomeType, Client, Expense, ExpenseType, Vendor } = require('../models');
 
-const tableOneData = require('./tableOneData.json');
-const tableTwoData = require('./tableTwoData.json');
+const userData = require('./userData.json');
+const incomeData = require('./incomeData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  // Write Seeds calling on data in required jsons above (lines 4 & 5)
+  // Seed User data
+  await User.bulkCreate(userData);
+
+  // Seed Income data
+  await Income.bulkCreate(incomeData);
+
+  // Seed Client data
+  await Client.bulkCreate(clientData);
+
+  // Seed Vendor data
+  await Vendor.bulkCreate(vendorData);
+
+  // Seed IncomeType data
+  await IncomeType.bulkCreate(incomeTypeData);
+
+  // Seed Expense data
+  await Expense.bulkCreate(expenseData);
+
+  // Seed ExpenseType data
+  await ExpenseType.bulkCreate(expenseTypeData);
 
   process.exit(0);
 };
