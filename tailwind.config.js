f89fa5987b7d65ca4fd02b9377a2c53cfc573632
mingosis <tom.piccirillo15@gmail.com>
2023-11-30 20:12:02 +1000

    css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/layouts/main.handlebars","./views/dashboard.handlebars","./views/login.handlebars","./views/signup.handlebars"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
