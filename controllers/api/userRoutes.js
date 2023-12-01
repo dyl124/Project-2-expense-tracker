edfef860ad3cfbcb0a503919d59dd04e0a8f99de
trnigg <tim@nigg.com.au>
2023-12-01 15:55:39 +0800

    seperated GET & render from POST request, fleshed-out /register

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,24 +1,27 @@
+// THis is my version with changes from Dylan's - May need to fix conflicts later
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Render the login page - separate from the login form submission (this should be a get request)
+// TODO - serve other pages (register/sign-up, etc.)
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Handle login form submissions as a post request
 router.post('/login', async (req, res) => {
   try {
-    res.render('main', {layout : 'login'});
-    const userData = await User.findOne({ where: { username: req.body.username } });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
@@ -34,18 +37,19 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
+router.post('/register', async (req, res) => {
+  try {
+    const newUser = await User.create({
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
     });
-  } else {
-    res.status(404).end();
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json(err);
   }
-});
-router.post('/register', async (req,res) =>
-{
-createNewUser = await firstName, lastName, email, password
 });
 
 module.exports = router;
