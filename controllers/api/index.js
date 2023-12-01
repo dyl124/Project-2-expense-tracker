d56d1740fce9cfcf05943dfa6af319f6181f00cb
dyl124 <142150017+dyl124@users.noreply.github.com>
2023-12-02 10:41:34 +1100

    working routes users

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const incomeRoutes = require('./income-routes');
 const expenseRoutes = require('./expense-routes');
-
+const dashBoard = require('./dashboard');
 router.use('/income', incomeRoutes);
 router.use('/expense', expenseRoutes);
+router.use('/dash', dashBoard);
+
 
 module.exports = router;
