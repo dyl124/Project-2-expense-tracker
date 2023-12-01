6de4cdc0829de4146dd6c9867c6071bf05501b54
Tim Nigg <tim@MacBook-Pro.local>
2023-12-01 09:44:24 +0800

    Added initial data requests

--- controllers/api/income-routes.js
+++ controllers/api/income-routes.js
@@ -1,8 +1,38 @@
 const router = require('express').Router();
-// Additional Requirements - such as Models
-// remember to get custom middleware if needed:
-// const withAuth = require('../utils/auth');
+const { User, Income, IncomeType, Client } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-// router. routes
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // Use req.session.user_id to get the currently logged-in user's ID
+    const userId = req.session.user_id;
+
+    // Find the user
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Fetch income data along with associated models - LATER WILL SELECT WHAT I WANT
+    const incomeData = await Income.findAll({
+      where: {
+        user_id: userId,
+      },
+      include: [
+        { model: IncomeType },
+        { model: Client },
+      ],
+    });
+
+    res.status(200).json({
+      user: user.toJSON(),
+      incomeData: incomeData.map((income) => income.toJSON()),
+    });
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 module.exports = router;
