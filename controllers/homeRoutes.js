20be6a813b57a28debb1b6b1d0161e7426a649e4
trnigg <tim@nigg.com.au>
2023-12-06 14:12:32 +0800

    passing relevant expense data now

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Income, IncomeType, Client, Expense } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/dashboard', withAuth, async (req, res) => {
+outer.get('/dashboard', withAuth, async (req, res) => {
   try {
     // Fetch the all income data from API for table in incomeTable partial
     const incomeData = await Income.findAll({
@@ -13,11 +13,27 @@ router.get('/dashboard', withAuth, async (req, res) => {
     });
     const incomes = incomeData.map((income) => income.get({ plain: true }));
 
+    // Fetch all expense data from API for table in expenseTable partial
+    const expenseData = await Expense.findAll({
+      where: {
+        user_id: req.session.user_id, // Make sure only the currently logged-in user's data is fetched.
+      },
+      include: [{ model: ExpenseType }, { model: Vendor }],
+    });
+    const expenses = expenseData.map((expense) => expense.get({ plain: true }));
+
     // function to also send the total income amount to the dashboard. Updated amount is handled in public/js/dashboard.js
-    let sumAmount = 0;
+    let incomeSumAmount = 0;
     incomes.forEach((income) => {
-      // for each income object, add the amount to the sumAmount variable and return as a float with 2 decimal places
-      sumAmount += parseFloat(income.amount);
+      // for each income object, add the amount to the incomeSumAmount variable and return as a float with 2 decimal places
+      incomeSumAmount += parseFloat(income.amount);
+    });
+
+    // function to also send the total expense amount to the dashboard. Updated amount is handled in public/js/dashboard.js
+    let expenseSumAmount = 0;
+    expenses.forEach((expense) => {
+      // for each expense object, add the amount to the expenseSumAmount variable and return as a float with 2 decimal places
+      expenseSumAmount += parseFloat(expense.amount);
     });
 
     // function to check for any incomes that are overdue (payment_status=false, due_date < today's date):
@@ -29,17 +45,29 @@ router.get('/dashboard', withAuth, async (req, res) => {
       }
     });
 
-    // pass the income data and sumAmount to the dashboard template for rendering the table
+    // function to check for any expenses that are overdue (payment_status=false, due_date < today's date):
+    let outstandingExpensesCount = 0;
+    expenses.forEach((expense) => {
+      // if expense payment_status is false and due_date is before today's date, increment outstandingExpensesCount
+      if (!expense.payment_status && new Date(expense.due_date) < new Date()) {
+        outstandingExpensesCount++;
+      }
+    });
+
+    // pass the income data, expense data, sumAmounts, and outstanding counts to the dashboard template for rendering the table
     res.render('dashboard', {
       logged_in: req.session.logged_in,
       incomes,
-      // pass sumAmount as 2 decimal-place float
-      sumAmount: sumAmount.toFixed(2),
-      // pass outstanding boolean to dashboard
+      expenses,
+      // pass sumAmounts as 2 decimal-place floats
+      incomeSumAmount: incomeSumAmount.toFixed(2),
+      expenseSumAmount: expenseSumAmount.toFixed(2),
+      // pass outstanding booleans to dashboard
       hasOutstandingIncomes: outstandingIncomesCount > 0,
-      // pass count to dashboard
+      hasOutstandingExpenses: outstandingExpensesCount > 0,
+      // pass counts to dashboard
       outstandingIncomesCount,
-      //
+      outstandingExpensesCount,
     });
   } catch (err) {
     res.status(500).json(err);
